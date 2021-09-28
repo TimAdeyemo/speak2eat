52f1ebaabe73c8fc66d5f045220c4e44b527cf57
TimAdeyemo <timadeyemo@gmail.com>
2021-09-28 17:21:22 -0400

    Init About, Cook, and Save pages

---
+++ client/src/components/Cook.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Cook = () => {
+  return (
+    <div className="Cook">
+      <p>This is the Cook Page.</p>
+    </div>
+  );
+};
+
+export default Cook;
---
+++ client/src/components/Save.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Save = () => {
+  return (
+    <div className="Save">
+      <p>This is the Save Page.</p>
+    </div>
+  );
+};
+
+export default Save;
